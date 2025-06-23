f76298180eb9e102cab7c0ef55e65234fe37142d
MichaelSalama265 <michaelsalama07@gmail.com>
2025-06-23 20:22:35 +0300

    create customNotesItem

---
+++ lib/Widgets/customNotesItem.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class NotesItem extends StatelessWidget {
+  const NotesItem({super.key});
+
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top: 24, bottom: 24, left: 16),
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: Text(
+              'Flutter Tips',
+              style: TextStyle(fontSize: 26, color: Colors.black),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(top: 20, bottom: 20),
+              child: Text(
+                'Dreams Come True ncjwnfkemf dfmekcme ',
+                style: TextStyle(color: Colors.black.withOpacity(.8)),
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.delete, color: Colors.black),
+              iconSize: 30,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 24),
+            child: Text(
+              'June 23 , 2025',
+              style: TextStyle(color: Colors.black.withOpacity(.8)),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/Widgets/notesViewBody.dart
+++ lib/Widgets/notesViewBody.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/Widgets/customAppBar.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/Widgets/customNotesItem.dart';
 
 class notesViewBody extends StatelessWidget {
   const notesViewBody({super.key});
@@ -7,13 +9,16 @@ class notesViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
+      padding: const EdgeInsets.symmetric(horizontal: 16),
       child: Column(
-        children: const[
-          SizedBox(height: 50,),
-         const customAppBar(),
+        children: const [
+          SizedBox(height: 50),
+          const customAppBar(),
+          SizedBox(height: 20,),
+          NotesItem(),
         ],
       ),
     );
   }
 }
+
