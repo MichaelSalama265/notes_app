cb50c858bfc1502e46ab22d6fa76bac90d367b59
MichaelSalama265 <michaelsalama07@gmail.com>
2025-06-29 00:18:04 +0300

    display notes part one

--- lib/Cubits/notes_cubit/notes_cubit.dart
+++ lib/Cubits/notes_cubit/notes_cubit.dart
@@ -8,15 +8,11 @@ part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
-  fetchAllNotes() async {
-    try {
-      var notesBox = Hive.box<NoteModel>(kNotesBox);
 
-      List<NoteModel> notes = notesBox.values.toList();
-      emit(NotesSuccess(notes));
-      
-    } catch (e) {
-      emit(NotesFaluire(e.toString()));
-    }
+  List<NoteModel>? notes;
+
+  fetchAllNotes() {
+    var notesBox = Hive.box<NoteModel>(kNotesBox);
+    notes = notesBox.values.toList();
   }
 }
--- lib/Widgets/notesViewBody.dart
+++ lib/Widgets/notesViewBody.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/Cubits/notes_cubit/notes_cubit.dart';
 import 'package:notes_app/Widgets/Notes_List.dart';
 import 'package:notes_app/Widgets/custom_AppBar.dart';
 
-class notesViewBody extends StatelessWidget {
+class notesViewBody extends StatefulWidget {
   const notesViewBody({super.key});
 
+  @override
+  State<notesViewBody> createState() => _notesViewBodyState();
+}
+
+class _notesViewBodyState extends State<notesViewBody> {
+  void initState(){
+    BlocProvider.of<NotesCubit>(context).fetchAllNotes();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Padding(
